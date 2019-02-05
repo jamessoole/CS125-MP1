92c7af300f1894d5e1079c641df9d0c6bd998935
James' Laptop <soole2@illinois.edu>
2019-02-05 14:44:21 -0600

    2

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -2,4 +2,18 @@ package edu.illinois.cs.cs125.spring2019.mp1.lib;
 
 /** Javadoc comment thing. */
 public class Transform {
+    /** Expand in the horizontal axis around the image center.
+     * @param amount these r words
+     * @param originalImage these r also words
+     * @return */
+    public static RGBAPixel[][] expandHorizontal(final RGBAPixel[][] originalImage, final int amount) {
+        return null;
+    }
+
+
+
+
+
+
+
 }
