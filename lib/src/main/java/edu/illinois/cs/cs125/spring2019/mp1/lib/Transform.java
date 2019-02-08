ce65246475c28d698d9cf24ec363d4f2e7e4f1d0
James' Laptop <soole2@illinois.edu>
2019-02-08 10:50:25 -0600

    3

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -9,9 +9,57 @@ public class Transform {
     public static RGBAPixel[][] expandHorizontal(final RGBAPixel[][] originalImage, final int amount) {
         return null;
     }
-
-
-
+    /** Expand in the vertical axis around the image center.
+     * @param amount these r words
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	expandVertical(final RGBAPixel[][] originalImage, final int amount) {
+        return null;
+    }
+    /** Flip the image on the horizontal axis across its center.
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	flipHorizontal(final RGBAPixel[][] originalImage) {
+        return null;
+    }
+    /** Flip the image on the vertical axis across its center.
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	flipVertical(final RGBAPixel[][] originalImage) {
+        return null;
+    }
+    /** Remove a green screen mask from an image.
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	greenScreen(final RGBAPixel[][] originalImage) {
+        return null;
+    }
+    /** Rotate the image left by 90 degrees around its center.
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	rotateLeft(final RGBAPixel[][] originalImage) {
+        return null;
+    }
+    /** Rotate the image left by 90 degrees around its center.
+     * @param originalImage these r also words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	rotateRight(final RGBAPixel[][] originalImage) {
+        return null;
+    }
+    /** Shift the image down by the specified amount.
+     * @param originalImage these r also words
+     * @param amount these r words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	shiftDown(final RGBAPixel[][] originalImage, final int amount) {
+        return null;
+    }
+    /** Shift the image left by the specified amount.
+     * @param originalImage these r also words
+     * @param amount these r words
+     * @return the expanded image*/
+    public static RGBAPixel[][]	shiftLeft(final RGBAPixel[][] originalImage, final int amount) {
+        return null;
+    }
 
 
 
