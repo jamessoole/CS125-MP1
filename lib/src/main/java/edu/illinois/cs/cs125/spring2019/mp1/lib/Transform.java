d2133280bf30c8b07e3d243628e3d242da30b797
James' Laptop <soole2@illinois.edu>
2019-02-11 10:40:32 -0600

    expand done

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -3,53 +3,81 @@ package edu.illinois.cs.cs125.spring2019.mp1.lib;
 
 /** Javadoc comment thing. */
 public class Transform {
-    /** maximum value.*/
+    /**
+     * maximum value.
+     */
     public static final int MAXVALUE = 255;
-    /** Expand in the horizontal axis around the image center.
-     * @param amount these r words
+
+    /**
+     * Expand in the horizontal axis around the image center.
+     *
+     * @param amount        these r words
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] expandHorizontal(final RGBAPixel[][] originalImage, final int amount) {
-        return null;
+        RGBAPixel[][] returnImage = RGBAPixel.copyArray(originalImage);
+        int shift = (originalImage.length * amount - originalImage.length) / 2;
+        for (int i = 0; i < originalImage.length; i++) {
+            for (int j = 0; j < originalImage[i].length; j++) {
+                returnImage[i][j] = originalImage[((shift + i) / amount) % originalImage.length][j];
+            }
+        }
+        return returnImage;
     }
 
-
-
-
-    /** Expand in the vertical axis around the image center.
-     * @param amount these r words
+    /**
+     * Expand in the vertical axis around the image center.
+     *
+     * @param amount        these r words
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] expandVertical(final RGBAPixel[][] originalImage, final int amount) {
-        return null;
+        RGBAPixel[][] returnImage = RGBAPixel.copyArray(originalImage);
+        int move = (originalImage[0].length * amount - originalImage[0].length) / 2;
+        for (int i = 0; i < originalImage.length; i++) {
+            for (int j = 0; j < originalImage[i].length; j++) {
+                returnImage[i][j] = originalImage[i][((move + j) / amount) % originalImage[i].length];
+            }
+        }
+        return returnImage;
     }
-    /** Flip the image on the horizontal axis across its center.
+
+    /**
+     * Flip the image on the horizontal axis across its center.
+     *
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] flipHorizontal(final RGBAPixel[][] originalImage) {
         return null;
     }
-    /** Flip the image on the vertical axis across its center.
+
+    /**
+     * Flip the image on the vertical axis across its center.
+     *
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] flipVertical(final RGBAPixel[][] originalImage) {
         return null;
     }
 
 
-
-
-
-    /** Remove a green screen mask from an image.
+    /**
+     * Remove a green screen mask from an image.
+     *
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] greenScreen(final RGBAPixel[][] originalImage) {
         RGBAPixel[][] returnImage = new RGBAPixel[originalImage.length][originalImage[0].length];
         for (int i = 0; i < originalImage.length; i++) {
             for (int j = 0; j < originalImage[i].length; j++) {
                 if (originalImage[i][j].getGreen() == MAXVALUE) {
                     returnImage[i][j] = RGBAPixel.getFillValue();
-                }   else {
+                } else {
                     returnImage[i][j] = originalImage[i][j];
                 }
             }
@@ -58,33 +86,47 @@ public class Transform {
     }
 
 
-
-
-
-    /** Rotate the image left by 90 degrees around its center.
+    /**
+     * Rotate the image left by 90 degrees around its center.
+     *
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] rotateLeft(final RGBAPixel[][] originalImage) {
         return null;
     }
-    /** Rotate the image left by 90 degrees around its center.
+
+    /**
+     * Rotate the image left by 90 degrees around its center.
+     *
      * @param originalImage these r also words
-     * @return the expanded image*/
+     * @return the expanded image
+     */
     public static RGBAPixel[][] rotateRight(final RGBAPixel[][] originalImage) {
         return null;
     }
-    /** Shift the image down by the specified amount.
+
+    /**
+     * Shift the image down by the specified amount.
+     *
      * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shiftDown(final RGBAPixel[][] originalImage, final int amount) {
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shiftDown(final RGBAPixel[][] originalImage,
+                                          final int amount) {
         return shiftUp(originalImage, amount * -1);
     }
-    /** Shift the image left by the specified amount.
+
+    /**
+     * Shift the image left by the specified amount.
+     *
      * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shiftLeft(final RGBAPixel[][] originalImage, final int amount) {
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shiftLeft(final RGBAPixel[][] originalImage,
+                                          final int amount) {
         RGBAPixel[][] returnImage = RGBAPixel.copyArray(originalImage);
         for (int i = 0; i < originalImage.length; i++) {
             for (int j = 0; j < originalImage[0].length; j++) {
@@ -97,21 +139,29 @@ public class Transform {
         }
         return returnImage;
     }
-    /** Shift the image right by the specified amount.
+
+    /**
+     * Shift the image right by the specified amount.
+     *
      * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shiftRight(final RGBAPixel[][] originalImage, final int amount) {
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shiftRight(final RGBAPixel[][] originalImage,
+                                           final int amount) {
         return shiftLeft(originalImage, amount * -1);
     }
 
 
-
-    /** Shift the image up by the specified amount.
+    /**
+     * Shift the image up by the specified amount.
+     *
      * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shiftUp(final RGBAPixel[][] originalImage, final int amount) {
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shiftUp(final RGBAPixel[][] originalImage,
+                                        final int amount) {
         RGBAPixel[][] returnImage = RGBAPixel.copyArray(originalImage);
         for (int i = 0; i < originalImage.length; i++) {
             for (int j = 0; j < originalImage[0].length; j++) {
@@ -126,21 +176,28 @@ public class Transform {
     }
 
 
-
-    /** Shrink in the horizontal axis around the image center.
+    /**
+     * Shrink in the horizontal axis around the image center.
+     *
      * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shrinkHorizontal(final RGBAPixel[][] originalImage, final int amount) {
-        return null;
-    }
-    /** Shrink in the vertical axis around the image center.
-     * @param originalImage these r also words
-     * @param amount these r words
-     * @return the expanded image*/
-    public static RGBAPixel[][] shrinkVertical(final RGBAPixel[][] originalImage, final int amount) {
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shrinkHorizontal(final RGBAPixel[][] originalImage,
+                                                 final int amount) {
         return null;
     }
 
+    /**
+     * Shrink in the vertical axis around the image center.
+     *
+     * @param originalImage these r also words
+     * @param amount        these r words
+     * @return the expanded image
+     */
+    public static RGBAPixel[][] shrinkVertical(final RGBAPixel[][] originalImage,
+                                               final int amount) {
+        return null;
+    }
 
 }
